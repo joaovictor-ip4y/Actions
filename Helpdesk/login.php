f6c7e87dcaf02f7126fd8b79f5d2d295aa37e3f9
joaovictor-ip4y <joao.victor@ip4y.com.br>
2025-01-13 10:48:28 -0300

    teste

--- Helpdesk/login.php
+++ Helpdesk/login.php
@@ -2,7 +2,7 @@
 session_start();
 include 'conexao.php';  // Incluindo a conexão com o banco de dados
 
-// Vswsdserificar se o formulário foi submetido
+// Verificar se o formulário foi submetido
 $erro = null;  // Inicializar variável de erro
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = trim($_POST['email']);  // Remover espaços em branco
