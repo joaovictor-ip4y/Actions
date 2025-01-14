43aee28f83afab57164afd806368077eba3c06a8
joaovictor-ip4y <joao.victor@ip4y.com.br>
2025-01-14 10:55:53 -0300

    teste

--- Helpdesk/tests/CalculatorTest.php
+++ Helpdesk/tests/CalculatorTest.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Calculator;
 use PHPUnit\Framework\TestCase;
+use Helpdesk\Calculator; // Usar o namespace correto
+
+require_once __DIR__ . '/../src/Calculator.php'; // Carregar a classe a partir de src/
 
 class CalculatorTest extends TestCase
 {
@@ -12,14 +14,13 @@ class CalculatorTest extends TestCase
         $this->calculator = new Calculator();
     }
 
-    public function testAdd()
+    public function testAddition()
     {
         $this->assertEquals(5, $this->calculator->add(2, 3));
     }
 
-    public function testSubtract()
+    public function testSubtraction()
     {
         $this->assertEquals(1, $this->calculator->subtract(3, 2));
     }
 }
-
